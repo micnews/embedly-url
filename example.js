10e37be835988377d0d1083dda604b49c54562d4
David Björklund <david.bjorklund@gmail.com>
2016-01-24 11:04:35 +0100

    example + readme

---
+++ example.js
@@ -0,0 +1,3 @@
+var embedlyUrl = require('./');
+
+console.log(embedlyUrl.parse('//cdn.embedly.com/widgets/media.html?src=http%3A%2F%2Fwww.youtube.com%2Fembed%2F3rS6mZUo3fg%3Ffeature%3Doembed&amp;url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D3rS6mZUo3fg&amp;image=http%3A%2F%2Fi.ytimg.com%2Fvi%2F3rS6mZUo3fg%2Fhqdefault.jpg&amp;key=25bf3602073943478e54668402a4f5a8&amp;type=text%2Fhtml&amp;schema=youtube'));
